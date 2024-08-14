58d939b313b294c5421f054729328869a8704025
GuilhermeNaves26 <mafraguilherme309@gmail.com>
2024-08-14 08:50:54 -0300

    upload do projeto

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello there!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}! http://localhost:${port}/`));
+
+app.get('/products', (req, res) => {
+    const products = [
+        { id: 1, name: 'hammer' },
+        { id: 2, name: 'screwdriver' },
+        { id: 3, name: 'wrench' },
+    ];
+
+    res.json(products);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}! http://localhost:${port}/`);
+});
